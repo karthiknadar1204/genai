ff54ec9e52b80a880ac6e401f7a9d71781426bd9
karthik <karthiknadar1204@gmail.com>
2024-09-04 11:29:54 +0530

    store embeddings to json file

--- chat-app/src/embeddings/main.ts
+++ chat-app/src/embeddings/main.ts
@@ -18,7 +18,39 @@ export const generateEmbeddings = async (input: string | string[]) => {
     return response;
   };
 
-generateEmbeddings(["Hello, world!","How are you?"]);
+
+  export const loadInputJson = <T>(fileName: string): T => {
+    const path = join(__dirname, fileName);
+    const rawInputData = readFileSync(path);
+    return JSON.parse(rawInputData.toString());
+  };
+
+
+  const saveEmbeddingsToJson = (embeddings: any, fileName: string) => {
+    const embeddingsString = JSON.stringify(embeddings);
+    const buffer = Buffer.from(embeddingsString);
+    const path = join(__dirname, fileName);
+    writeFileSync(path, buffer);
+    console.log(`Embeddings saved to ${path}`);
+  };
+
+
+  const main = async () => {
+    const input = loadInputJson<string[]>("input.json");
+    const embeddings = await generateEmbeddings(input);
+    const dataWithEmbeddings: DataWithEmbeddings[] = input.map(
+      (input, index) => ({
+        input,
+        embeddings: embeddings.data[index].embedding,
+      })
+    );
+    saveEmbeddingsToJson(dataWithEmbeddings, "dataWithEmbeddings.json");
+  };
+
+  main();
+
+
+// generateEmbeddings(["Hello, world!","How are you?"]);
 
 
 
