75967f63bd0193224281ba6968da6dfac62703fb
karthik <karthiknadar1204@gmail.com>
2024-09-04 11:07:34 +0530

    embeddings generate test

---
+++ chat-app/src/embeddings/main.ts
@@ -0,0 +1,26 @@
+import { readFileSync, writeFileSync } from "fs";
+import OpenAI from "openai";
+import { join } from "path";
+
+export type DataWithEmbeddings = {
+    input: string;
+    embeddings: number[];
+  };
+
+const openai = new OpenAI();
+
+export const generateEmbeddings = async (input: string | string[]) => {
+    const response = await openai.embeddings.create({
+      input: input,
+      model: "text-embedding-3-small",
+    });
+    console.log(response.data);
+    return response;
+  };
+
+generateEmbeddings("Hello, world!");
+
+
+
+
+
